e747032d9f6a2bde2ed5eccee926b30f82df1c3d
YoungYoung619 <940752885@qq.com>
2019-09-17 12:32:05 +0800

    'add_two_nums'

--- problem/2_add_two_numbers.py
+++ problem/2_add_two_numbers.py
@@ -8,6 +8,21 @@ Description :
 Author：Team Li
 """
 
+"""
+2. Add Two Numbers
+Medium
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
 class ListNode(object):
     def __init__(self, x):
         self.val = x
@@ -20,8 +35,15 @@ def addTwoNumbers(l1, l2):
     :type l2: ListNode
     :rtype: ListNode
     """
-
+    ## time O(n), but code not beautiful...
     def sub_add(node, extra_add, l1_node, l2_node):
+        """
+        Args:
+            node: a new node, which would be used to store the l1_node.val + l2_node.val + extra_add
+            extra_add: indicate last add whether bigger than 10
+            l1_node: current node of l1
+            l2_node: current node of l2
+        """
         if l1_node or l2_node:
             if not l1_node:
                 l1_val = 0
